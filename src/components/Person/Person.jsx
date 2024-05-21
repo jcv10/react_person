42e3dea4615625bbf1a57040e90d0073f51d14a2
Julio Velasco <juliocvelasco90@gmail.com>
2024-05-21 12:27:41 -0400

    Implemented Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let partnerName = 'I am not married';
+
+  if (person.isMarried) {
+    partnerName = `${person.partnerName} is my ${Person.sex === 'f' ? 'husband' : 'wife'}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      <p className="Person__age">I am {person.age}</p>
+      <p className="Person__partner">{partnerName}</p>
+    </section>
+  );
+};
